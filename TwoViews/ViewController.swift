d3d98299c54fa451d3333d45599df5d3d0821d3e
NatashaOvdina <natashaovdina@yahoo.com>
2023-10-31 15:47:23 +0300

    Initial Commit

---
+++ TwoViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoViews
+//
+//  Created by Natalia Ovdina on 31.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
